1f9a6e9e7235312389b7881abf8e02267695ae36
nikjdwhite <ndjeannio@gmail.com>
2017-10-06 11:32:07 -0500

    Restructured item and collections browse

--- studiobase/collections/browse.php
+++ studiobase/collections/browse.php
@@ -8,37 +8,16 @@
     
     <div class="browse-collections">
         <?php if ($total_results > 0): ?>
-            <div class="browse-collections-header hidden-xs">
-                <div class="row">
-                    <div class="col-sm-3 col-sm-offset-2">
-                        <?php echo browse_sort_links(array('Title'=>'Dublin Core,Title'), array('')); ?>
-                    </div>
-                    <div class="col-sm-3">
-                        <?php echo browse_sort_links(array('Creator'=>'Dublin Core,Contributor'), array('')); ?>
-                    </div>
-                    <div class="col-sm-4">
-                        Description
-                    </div>
-                </div>
-            </div>
         
             <?php foreach (loop('collections') as $collection): ?>
-                <div class="collection">
                     <div class="row">
-                        <div class="col-sm-2">
+                        <div class="col-sm-3">
                             <?php if ($collectionImage = record_image('collection', 'square_thumbnail')): ?>
                                 <?php echo link_to_collection($collectionImage, array('class' => 'image')); ?>
                             <?php endif; ?>
                         </div>
-                        <div class="col-sm-3">
-                            <?php echo link_to_collection(); ?>
-                        </div>
-                        <div class="col-sm-3">
-                            <?php if ($collection->hasContributor()): ?>
-                                <?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?>
-                            <?php endif; ?>
-                        </div>
-                        <div class="col-sm-4">
+                        <div class="col-sm-9">
+                            <h2><?php echo link_to_collection(); ?></h2>
                             <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
                                 <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?>
                             <?php endif; ?>
@@ -46,7 +25,6 @@
                     
                         <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
                     </div>
-                </div>
             <?php endforeach; ?>
         <?php else : ?>
             <p><?php echo 'No collections added, yet.'; ?></p>
--- studiobase/items/browse.php
+++ studiobase/items/browse.php
@@ -8,57 +8,23 @@
     <?php $subnav = public_nav_items(); echo $subnav->setUlClass('nav nav-pills'); ?>
     <hr>    
 
-    <div class="browse-items">
+    <div class="browse-items clearfix">
         <?php if ($total_results > 0): ?>
-        <?php
-            $sortLinks[__('Title')] = 'Dublin Core,Title';
-            $sortLinks[__('Creator')] = 'Dublin Core,Creator';
-            ?>
-            <div class="browse-items-header hidden-xs">
-                <div class="row">
-                    <div class="col-sm-3 col-sm-offset-2 col-md-2 col-md-offset-2">
-                        <?php echo browse_sort_links(array('Title'=>'Dublin Core,Title'), array('')); ?>
-                    </div>
-                    <div class="col-sm-3 col-md-2">
-                        <?php echo browse_sort_links(array('Creator'=>'Dublin Core,Creator'), array('')); ?>
-                    </div>
-                    <div class="hidden-sm col-md-2">
-                        Subject
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        Description
-                    </div>
-                </div>
-            </div>
-        
+
             <?php foreach (loop('items') as $item): ?>
-            <div class="item">
                 <div class="row">
-                    <div class="col-sm-2 col-md-2">
-                        <?php $image = $item->Files; ?>
-                        <?php if ($image) {
-                                echo link_to_item('<div style="background-image: url(' . file_display_url($image[0], 'original') . ');" class="img"></div>');
-                            } else {
-                                echo link_to_item('<div style="background-image: url(' . img('defaultImage@2x.jpg') . ');" class="img"></div>');
-                            }
-                        ?>
+                    <div class="col-sm-3">
+                        <?php echo link_to_item(item_image('square_thumbnail', array('alt' => metadata('item', array('Dublin Core', 'Title'))))); ?>
                     </div>
-                    <div class="col-sm-3 col-md-2">
-                        <?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?>
-                    </div>
-                    <div class="col-sm-3 col-md-2">
-                        <?php echo metadata('item', array('Dublin Core', 'Creator')); ?>
-                    </div>
-                    <div class="hidden-sm col-md-2">
-                        <?php echo metadata('item', array('Dublin Core', 'Subject')); ?>
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        <?php echo metadata('item', array('Dublin Core', 'Description'), array('snippet'=>150)); ?>
+                    <div class="col-sm-9">
+                        <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
+
+                       <?php echo text_to_paragraphs(metadata('item', array('Dublin Core', 'Description'), array('snippet'=>150))); ?>
                     </div>
                 
                     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
-                </div>
-            </div>
+				</div>
+
             <?php endforeach; ?>
             <div id="outputs">
                 <span class="outputs-label"><?php echo __('Output Formats'); ?></span>
