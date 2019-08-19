96891ba1449185464e3097d0356584dadb7c8ded
Matthew Butler <matthewtbutler@gmail.com>
2019-08-19 13:52:22 -0500

    image viewer for jpgs and pdfs

--- lancelot/index.php
+++ lancelot/index.php
@@ -6,23 +6,23 @@ echo head(array(
 ?>
 
 <div class="container max-width-900">
-		<div class="row pt-5 pb-5 home">
-			<div class="col-sm-6 col-12 mb-4">
-				<h2><em>Welcome</em></h2>
-				<p>Welcome to the official database for the William Morris Archive, a text-searchable scholarly edition of the poetry and selected prose of William Morris.</p>
-				<a href="#" class="button1">Titles</a>
-				<a href="#" class="button2">Advanced Search</a>
-				<a href="#" class="button3">Resources</a>
-				<a href="#" class="button4">Featured Exhibit</a>
-			</div>
-			<div class="col-sm-6 col-12">
-				<div>
-					<img src="<?php echo img('william-morris.jpg'); ?>" class="img-fluid" alt="William Morris, portrait by G.F. Watts, 1870"/>
-					<p class="caption">William Morris, portrait by G.F. Watts, 1870</p>
-				</div>
+	<div class="row pt-5 pb-5 home">
+		<div class="col-sm-6 col-12 mb-4">
+			<h2><em>Welcome</em></h2>
+			<p>Welcome to the official database for the William Morris Archive, a text-searchable scholarly edition of the poetry and selected prose of William Morris.</p>
+			<a href="#" class="button1">Titles</a>
+			<a href="#" class="button2">Advanced Search</a>
+			<a href="#" class="button3">Resources</a>
+			<a href="#" class="button4">Featured Exhibit</a>
+		</div>
+		<div class="col-sm-6 col-12">
+			<div>
+				<img src="<?php echo img('william-morris.jpg'); ?>" class="img-fluid" alt="William Morris, portrait by G.F. Watts, 1870"/>
+				<p class="caption">William Morris, portrait by G.F. Watts, 1870</p>
 			</div>
 		</div>
 	</div>
+</div>
 
 <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
